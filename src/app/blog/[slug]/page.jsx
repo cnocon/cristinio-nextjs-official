1b7b02811c33b2a18e62b9703429032201a41b95
Cristin O’Connor <her@cristin.io>
2026-01-10 14:10:06 -0700

    Installed react-syntax-highlighter for blog posts and added dynamic blog post routes

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,4 +1,12 @@
+import styles from "../../page.module.scss";
 export default async function Page({ params }) {
-  const { slug } = await params
-  return <div>My Post: {slug}</div>
+  const { slug } = await params;
+
+  return (
+    <section className={styles.page}>
+      <header className={styles['header']}>
+        <h2 className={styles['page-title']}>{slug}</h2>
+      </header>
+    </section>
+  )
 }
