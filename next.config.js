303ff5608a29739e56c71dda10ae74d80f9e6909
Cristin O’Connor <her@cristin.io>
2026-01-10 09:39:02 -0700

    Installed SASS, updated style files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  sassOptions: {}
 };
 
 export default nextConfig;
