b481de398dfcf5c7ca9ff4053520dce0954cc84f
Cristin O’Connor <her@cristin.io>
2026-01-26 16:50:12 -0700

    Working homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,17 +7,19 @@ import ConnectRow from "./ui/connect-row";
 export default function Page() {
   return (
     <section className={`${pageStyles.page} ${styles.homePage}`}>
-      <div className={styles.welcome}>
+      <div className={styles.welcomeSection}>
         <blockquote className={styles.bannerBlockquote}>
           <p><ImQuotesLeft />How we spend our days is, of course, how we spend our lives. What we do with this hour, and that one, is what we are doing.<ImQuotesRight /></p>
           <p><cite>— Annie Dillard</cite></p>
 
         </blockquote>
       </div>
+
       <div className={styles.postListSection}>
         <header><h2><span>Latest Posts</span></h2></header>
         <PostList />
       </div>
+
       <div className={styles.connectSection}>
         <header><h2><span>Connect</span></h2></header>
         <ConnectRow />
